a491d6465e80ccc920b0c2fccc1e9c6b014fff10
ChrisOnMac <20260809+schmizzel@users.noreply.github.com>
2023-03-15 22:05:26 +0100

    added admin page

---
+++ src/routes/admin/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+    function openBox() {
+        fetch('http://192.168.1.1:3000/api/open', { method: 'GET' })
+            .then(res => res.json())
+            .then(json => console.log('box opened'))
+            .catch(err => console.error(err))
+    }
+
+    function setState(state) {
+        fetch('http://192.168.1.1:3000/api/setState', { 
+            method: 'POST', 
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                state,
+            })
+        })
+            .then(res => res.json())
+            .then(json => console.log('box opened'))
+            .catch(err => console.error(err)) 
+    }
+</script>
+
+<div class="absolute w-full h-full flex justify-center items-center bg-windows-background"> 
+    <div class="flex gap-4 text-3xl">
+        <button class="btn bg-windows-gray-dark" on:click={openBox}>Open box</button>
+        <button class="btn bg-windows-gray-dark" on:click={() => setState('Ready')}>Set Ready</button>
+        <button class="btn bg-windows-gray-dark" on:click={() => setState('Updating')}>Set Updating</button>
+    </div>
+</div>
+
